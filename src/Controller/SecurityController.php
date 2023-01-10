3565953532ff7b4c8e54ec3aa7038a6d87125394
Rachel <rachel.bouhier@outlook.com>
2023-01-10 13:17:39 +0100

    create simple homepage and navbar

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,7 +6,6 @@ use App\Form\LoginType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
