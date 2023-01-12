eaf26d957238a986060d7bc83f7cf31aada25037
Rachel <rachel.bouhier@outlook.com>
2023-01-12 12:05:51 +0100

    add simple static page for order button

---
+++ src/Controller/OrderController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class OrderController extends AbstractController
+{
+    #[Route('/order', name: 'order')]
+    public function order(): Response
+    {
+        return $this->render('order.html.twig', []);
+    }
+}
