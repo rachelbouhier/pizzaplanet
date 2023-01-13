65e2c29f6a89e304933451e298c63f8bb7bc5463
Rachel <rachel.bouhier@outlook.com>
2023-01-13 18:07:03 +0100

    add route for create and view

---
+++ src/Controller/IngredientController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\IngredientType;
+use Doctrine\ORM\EntityManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IngredientController extends AbstractController
+{
+    #[Route('/admin/ingredient/create', name: 'ingredient_create')]
+    public function create(Request $request, EntityManager $em): Response
+    {
+        $form = $this->createForm(IngredientType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $ingredient = $form->getData();
+
+            $em->persist($ingredient);
+            $em->flush();
+        }
+
+        $formView = $form->createView();
+        
+        return $this->render('ingredient/create.html.twig', [
+            'formView' => $formView
+        ]);
+    }
+}
