3072977dc4349b85f26abc097dfb9c8d6cc1fbcd
Rachel <rachel.bouhier@outlook.com>
2023-01-13 19:31:56 +0100

    add redirect route after creation

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use App\Form\IngredientType;
-use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,7 +12,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class IngredientController extends AbstractController
 {
     #[Route('/admin/ingredient/create', name: 'ingredient_create')]
-    public function create(Request $request, EntityManager $em): Response
+    public function create(Request $request, EntityManagerInterface $em): Response
     {
         $form = $this->createForm(IngredientType::class);
 
@@ -23,6 +23,8 @@ class IngredientController extends AbstractController
 
             $em->persist($ingredient);
             $em->flush();
+
+            return $this->redirectToRoute('homepage');
         }
 
         $formView = $form->createView();
